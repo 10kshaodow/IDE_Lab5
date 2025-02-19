eb24ebce9eff04175cd4cd8f39121edc9224be54
Juliana Gentile <90779400+julianagentile@users.noreply.github.com>
2025-02-19 13:28:30 -0500

    Created switches.h

---
+++ switches.h
@@ -0,0 +1,13 @@
+#ifndef SWITCHES_H
+#define SWITCHES_H
+
+#include "msp.h"
+#include "Common.h"
+
+// Function Prototypes
+void Switch1_Init(void);
+void Switch2_Init(void);
+BOOLEAN Switch1_Pressed(void);
+BOOLEAN Switch2_Pressed(void);
+
+#endif /* SWITCHES_H */
